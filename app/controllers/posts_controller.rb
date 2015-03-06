c30ad0b6af59f129b5d4c34f03e541b1a9479830
Sean Yamaguchi <your.email>
2015-03-06 21:35:23 +1100

    検索機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @q = Post.search(params[:q])
+    @posts = @q.result(distinct: true)
   end
 
   # GET /posts/1
