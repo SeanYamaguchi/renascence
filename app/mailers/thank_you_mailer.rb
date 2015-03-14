c7b9a3b3c90aa16b9d20192a469927863c323afe
Sean Yamaguchi <your.email>
2015-03-14 18:47:52 +1100

    mailer がおかしいので

--- app/mailers/thank_you_mailer.rb
+++ app/mailers/thank_you_mailer.rb
@@ -1,15 +1,15 @@
 class ThankYouMailer < ActionMailer::Base
-  default from: "info@psara.co.jp"
-  default cc: "sean@psara.co.jp"
+ # default from: "info@psara.co.jp"
+  #default cc: "sean@psara.co.jp"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.thank_you_mailer.sendmail_confirm.subject
   #
-  def sendmail_confirm(user)
-    @user = user
-    mail(to: user.email),
-      subject: 'ユーザー登録ありがとうございました。')
-  end
+  #def sendmail_confirm(user)
+   # @user = user
+    #mail(to: user.email),
+     # subject: 'ユーザー登録ありがとうございました。')
+  #end
 end
