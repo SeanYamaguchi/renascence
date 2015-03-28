19a343859afddf39fa1445fe095bb5e3038ccfd0
Sean Yamaguchi <your.email>
2015-03-28 22:29:49 +1100

    動くかどうか良くわかんないけど

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,3 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
