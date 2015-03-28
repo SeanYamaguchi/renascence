84584d3d1b81a277a5cd6c8cce051f2167f790e6
Sean Yamaguchi <your.email>
2015-03-28 19:08:36 +1100

    emailのvalidatorを追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -5,5 +5,6 @@ class Inquiry
  
   validates :name, :presence => {:message => '名前を入力してください'}
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
-  validates :message, :presence => {:message => 'お問い合わせ内容を記載して下さい'}
+  validates :message, :presence => {:message => 'お問い合わせ内容を記載して下さい'}, :email => true
+
 end
