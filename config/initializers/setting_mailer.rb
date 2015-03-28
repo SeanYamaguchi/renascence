0c5ee9afb7a7f7538aea7979a9666a381129692e
Sean Yamaguchi <your.email>
2015-03-28 18:35:06 +1100

    setting_mailerを変更

--- config/initializers/setting_mailer.rb
+++ config/initializers/setting_mailer.rb
@@ -1,9 +1,13 @@
-#  ActionMailer::Base.smtp_setting = {
- # :address => 'smtp.sendgrid.new',
-#  :port => '587',
- # :authentication => :plain,
-#  :user_name => 'app34862435@heroku.com',
- # :password => 'kbrhc1jk',
-#  :domain => 'psara-institute.com/',
- # :enable_starttls_auto => true
-# }
+
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_url_options = { :host => 'localhose:3000'}
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_setting = {
+  :address => 'smtp.sendgrid.new',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => 'app34862435@heroku.com',
+  :password => 'kbrhc1jk',
+  :domain => 'psara-institute.com/',
+  :enable_starttls_auto => true
+}
