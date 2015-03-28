eee5a8c052698fbb857566679fd14d1df7ec161f
Sean Yamaguchi <your.email>
2015-03-28 21:34:53 +1100

    port変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,13 +51,15 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhose:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address => 'smtp.sendgrid.new',
+  :port => '587',
   :authentication => :plain,
   :user_name => 'app34862435@heroku.com',
   :password => 'kbrhc1jk',
   :domain => 'psara-institute.com/',
-  :enable_starttls_auto => false
+  :enable_starttls_auto => true
 }
 end
