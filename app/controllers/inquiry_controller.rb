c63e52ed115d4d55e426c3a6e75a483bc1a5b0a6
Sean Yamaguchi <your.email>
2015-03-28 12:34:39 +1100

    heokuでメッセージ送信機能を実装

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,13 +1,14 @@
 class InquiryController < ApplicationController
   def index
     # 入力画面を表示
+    @user = User.new(current_user)
     @inquiry = Inquiry.new
     render :action => 'index'
   end
  
   def confirm
     # 入力値のチェック
-    @inquiry = Inquiry.new(params[:inquiry])
+    @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.valid?
       # OK。確認画面を表示
       render :action => 'confirm'
@@ -19,10 +20,15 @@ class InquiryController < ApplicationController
  
   def thanks
     # メール送信
-    @inquiry = Inquiry.new(params[:inquiry])
+    @inquiry = Inquiry.new(inquiry_params)
     InquiryMailer.received_email(@inquiry).deliver
  
     # 完了画面を表示
     render :action => 'thanks'
+  end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :message)
   end
 end
---
+++ redis-2.8.13/src/release.h
@@ -0,0 +1,3 @@
+#define REDIS_GIT_SHA1 "d3116949"
+#define REDIS_GIT_DIRTY "       0"
+#define REDIS_BUILD_ID "Yamaguchi-no-MacBook-Pro-2.local-1427307565"
