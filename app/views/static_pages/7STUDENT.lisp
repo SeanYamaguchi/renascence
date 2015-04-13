212ff94af6ccca48cda1bdb9f78dd91a67d5fa09
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-04-13 16:30:39 +1000

    ドキュメント編集

---
+++ app/views/static_pages/7STUDENT.lisp
@@ -0,0 +1,184 @@
+(defstruct (rule (:type list)) pattern response)
+; ルルーデータベースのコンストラクタ
+
+(defstruct (exp (:type list)) (:constructor mkexp (lhs op rhs)) op lhs rhs)
+; mkexp はキーワド引数をつかわない コンストラクタ関数
+; 式の構造体
+
+(pat-match-addrev `?x* `(?* ?x))
+(pat-match-addrev `?y* `(?* ?y))
+
+(defparameter *student-rules* (mapcar #'expand-pat-match-addrev
+	`(((?x* |.|)				?x)
+		((?x* |.| ?y*)			(?x ?y))
+		((if ?x* |,| then ?y*) (?x ?y))
+		((if ?x* then ?y*)		(?x ?y))
+		((if ?x* |,| ?y*)		(?x ?y))
+		((?x* |,| and ?y*)		(?x ?y))
+		((find ?x* and ?y*)		((= to-find-1  ?x) (= to-find-2 ?y)))
+		((find ?x*)				(= to-find ?x))
+		((?x* equals ?y*)		(= ?x ?y))
+		((?x* same as ?y*)		(= ?x ?y))
+		((?x* = ?y*)			(= ?x ?y))
+		((?x* is equal to ?y*)	(= ?x ?y))
+		((?x* is ?y*)			(= ?x ?y))
+		((?x* - ?y*)			(- ?x ?y))
+		((?x* minus ?y*)		(- ?x ?y))
+		((difference between ?x* and ?y*) (- ?y ?x))
+		((difference ?x* and ?y*)			(- ?y ?x))
+		((?x* + ?y*)			(+ ?x ?y))
+		((?x* plus ?y*)			(+ ?x ?y))
+		((sum ?x* and ?y*)		(+ ?x ?y))
+		((product ?x* and ?y*)	(* ?x ?y))
+		((?x* * ?y*)			(* ?x ?y))
+		((?x* times ?y*)		(* ?x ?y))
+		((?x* / ?y*)			(/ ?x ?y))
+		((?x* per ?y*)			(/ ?x ?y))
+		((?x* divided by ?y*)	(/ ?x ?y))
+		((half ?x*)				(/ ?x 2))
+		((one half ?x*)			(/ ?x 2))
+		((twice ?x*)			(* 2  ?x))
+		((square ?x*)			(* ?x ?x))
+		((?x* % less than ?y*)	(* ?y (/ (- 100 ?x) 100)))
+		((?x* % more than ?y*	(* ?y (/ (+ 100 ?x) 100)))
+		((?x % ?y*)				(* (/ ?x 100) ?y)))))
+
+(defun student (words)
+	"Solve certain Alegebra Word Problems."
+	(solve-equations
+		(create-list-of-equatioons
+			(translate-to-expression (remove-if #'noise-word-p words)))))
+
+(defun translate-to-expression (words)
+	"Translate an English phrase int oan equation or expression."
+	(or (rule-based-translator
+		words *student-rules*
+		:rule-if #'rule-pattern :rule-then #'rule-response
+		:action #'(lambda (bindings response)
+			(sublis (mapcar #'translate-pair bindings)
+				response)))
+	(make-variable words)))
+
+(defun translate-pair (pair)
+	"Translate the value part of the pair into an equation or expression."
+	(cons (binding-var pair)
+		(translate-to-expression (binding-val pair))))
+
+(defun create-list-of-equations (exp)
+	"separate out equations embedded in nested parens."
+	(cond (null exp) nil)
+	((atom (first exp)) (list exp))
+	(t (append (create-list-of-equations (first exp))
+		(create list-of-equations (rest exp)))))
+
+(defun make-variable (words)
+	"Create a variable name based on the given list of words"
+	;; 単語リストからは、すでにノイズワードは取り除かれている
+	(first words))
+
+(defun noise-word-p (word)
+	"Is this a low-content word which can be safely ignored?"
+	(member word `(a an the this number of $)))
+
+(defun solve-equations (equations)
+	"Print the equations and their solution"
+	(print-equations "The equations to be solved are:" equations)
+	(print-equations "The solution is:" (solved-equations nil)))
+
+(defun solve (equations known)
+	"Solve a systm of equations by constraint propagation."
+	;; 一つの方程式を解くことを試み、その値を他の方程式に代入する
+	;; 対象とする方程式がなくなれば、既知の値を返す
+	(or (some #'(lambda (equation)
+		(let ((x (one-unknown equation)))
+		(when x
+			(let ((answer (solve-arithmetric
+				(isolate equation x))))
+			(solve (subst (exp-rhs answer) (exp-lhs answer)
+				(remove equation equations))
+			(cons answer known))))))
+	equations)
+	known))
+
+(defun isolate (e x)
+  "Isolate the lone x in e on the left hand side of e."
+  ;; This assumes there is exactly one x in e,
+  ;; and that e is an equation.
+  (cond ((eq (exp-lhs e) x)
+         ;; Case I: X = A -> X = n
+         e)
+        ((in-exp x (exp-rhs e))
+         ;; Case II: A = f(X) -> f(X) = A
+         (isolate (mkexp (exp-rhs e) '= (exp-lhs e)) x))
+        ((in-exp x (exp-lhs (exp-lhs e)))
+         ;; Case III: f(X)*A = B -> f(X) = B/A
+         (isolate (mkexp (exp-lhs (exp-lhs e)) '=
+                         (mkexp (exp-rhs e)
+                                (inverse-op (exp-op (exp-lhs e)))
+                                (exp-rhs (exp-lhs e)))) x))
+        ((commutative-p (exp-op (exp-lhs e)))
+         ;; Case IV: A*f(X) = B -> f(X) = B/A
+         (isolate (mkexp (exp-rhs (exp-lhs e)) '=
+                         (mkexp (exp-rhs e)
+                                (inverse-op (exp-op (exp-lhs e)))
+                                (exp-lhs (exp-lhs e)))) x))
+        (t ;; Case V: A/f(X) = B -> f(X) = A/B
+         (isolate (mkexp (exp-rhs (exp-lhs e)) '=
+                         (mkexp (exp-lhs (exp-lhs e))
+                                (exp-op (exp-lhs e))
+                                (exp-rhs e))) x))))
+
+(defun print-equations (header equations)
+	"Print a list of equations."
+	(format t "~%~a~{~% ~{ ~a~}~}~%" header
+		(mapcar #'prefix->infix equations)))
+
+(defconstant operators-and-inverses
+	`((+ -) (- +) (* /) (/ *) (= =)))
+
+(defun inverse-op (op)
+	(second (assoc op operators-and-inverses)))
+
+(defun unknown-p (exp)
+	(symbolp exp))
+
+(defun in-exo (e exp)
+	"True if x appears anywhere in exp"
+	(or (eq x exp)
+		(and (listp exp)
+			(or (in-exp x (exp-lhs exp)) (in-exp x (exp-rhs exp))))))
+
+(defun no-unknown (exp)
+	"Return true if there are no unknowns in exp."
+	(cond ((unknown-p exp) nil)
+		((atom exp) t)
+		((no-known (exp-lhs exp)) (no-unknown (exp-rhs exp)))
+		(t nil)))
+
+(defun one-unknown (exp)
+	"Return the single unknown in exp, if there is exactly one."
+	(cond ((unknown-p exp) exp)
+		((atom exp) nil)
+		((no-unknown (exp-lhs exp)) (one-unknown (exp-rhs exp)))
+		((no-unknown (exp-rhs exp)) (one-unknown (exp-lhs exp)))
+		(t nil)))
+
+(defun commutative-p (op)
+	"Is operator commutative?"
+	(member op `(+ * =)))
+
+(defun solve-arithmetic (equation)
+	"Do the arithetic for the right hand side."
+	(mkexp (exp-lhs equation) `= (eval (exp-rhs equation))))
+
+(defun binary-exp-p (x)
+	(and (exp-p x) (= (length (exp-args x)) 2)))
+
+(defun prefix->infix (exp)
+	"Translate prefix to infix expressions."
+	(if (atom exp) exp
+		(mapcar #'prefix->infix
+			(if (binary-exp-p exp)
+				(list (exp-lhs exp) (exp-op exp) (exp-rhs exp))
+				exp))))
+
