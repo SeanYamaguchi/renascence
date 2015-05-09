67bb8d6898b48cfb19f924a0fa70613f6d3138d4
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-05-09 20:34:48 +0900

    活動内容更新 abc123 =~ /¥d+/ abc123 =~ /¥d+/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,34 @@ $(document).scroll(function() {
     }
 });
 
+$(document).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll <= 100) {
+    	$(".head-link-1").removeClass("head-gradation-1");
+    	$(".head-link-1").addClass("sc-purple");
+    	$(".head-link-2").removeClass("head-gradation-2");
+    	$(".head-link-2").addClass("sc-red");
+    	$(".head-link-3").removeClass("head-gradation-3");
+    	$(".head-link-3").addClass("sc-green");
+    	$(".head-link-4").removeClass("head-gradation-4");
+    	$(".head-link-4").addClass("sc-yellow");
+     	$(".head-link-5").removeClass("head-gradation-5");
+    	$(".head-link-5").addClass("sc-blue");
+    	$(".head-link-6").removeClass("head-gradation-6");
+    	$(".head-link-6").addClass("sc-swan");
+    } else {
+    	$(".head-link-1").removeClass("sc-purple");
+        $(".head-link-1").addClass("head-gradation-1");
+    	$(".head-link-2").removeClass("sc-red");
+        $(".head-link-2").addClass("head-gradation-2");
+    	$(".head-link-3").removeClass("sc-green");
+        $(".head-link-3").addClass("head-gradation-3");
+    	$(".head-link-4").removeClass("sc-yellow");
+        $(".head-link-4").addClass("head-gradation-4");
+    	$(".head-link-5").removeClass("sc-blue");
+        $(".head-link-5").addClass("head-gradation-5");
+    	$(".head-link-6").removeClass("sc-swan");
+        $(".head-link-6").addClass("head-gradation-6");
+    }
+});
