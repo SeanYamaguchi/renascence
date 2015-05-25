02afbf6ceb680e7c5273f3353eb4c2b212ecefad
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-05-26 01:20:42 +0900

    twitter-omniauth 残りのエラー対処

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,14 @@
 class OmniauthCallbacksController < DeviseController
   prepend_before_filter { request.env["devise.skip_timeout"] = true }
   def all
-	user = User.from_omniauth(request.env["omniauth.auth"])
-	if user.persisted?
-	  flash.notice = "Logged in successfully"
-	  sign_in_and_redirect user
-	else
-	  session["devise.user_attributes"] = user.attributes
-	  redirect_to new_user_registration_url
-	end
+	  user = User.from_omniauth(request.env["omniauth.auth"])
+	  if user.persisted?
+	    flash.notice = "Logged in successfully"
+	    sign_in_and_redirect user
+	  else
+	    session["devise.user_attributes"] = user.attributes
+	    redirect_to new_user_registration_url
+	  end
   end
 
   def passthru
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :omniauthable, omniauth_providers: [:twitter]
 
   def self.from_omniauth(auth)
-  	where(auth.slice(:provider, :uid)).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   	  user.name = auth.info.nickname
   	  user.email = auth.info.email
   	end
