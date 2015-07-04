3678a4ed732679e0619dacc3564b915edaefa723
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-07-04 15:50:29 +0900

    change the name of host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,15 +52,15 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'psara-institute.com'}
+  config.action_mailer.default_url_options = { :host => 'psara-institute.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
-  :authentication => "plain",
+  :authentication => "login",
   :user_name => 'app38502952@heroku.com',
   :password => 'kbqqyikp1540',
-  :domain => 'heroku.com',
+  :domain => 'heroku.com/',
   :enable_starttls_auto => true
 }
 end
